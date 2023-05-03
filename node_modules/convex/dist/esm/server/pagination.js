65822123e3ac3da666d6fe2992c6ea4c11631efe
Kim-Quirk <kimblackaz@gmail.com>
2023-05-02 22:06:30 -0600

    front end only

--- node_modules/convex/dist/esm/server/pagination.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=pagination.js.map
--- script.js
+++
@@ -1,5 +0,0 @@
-const { ConvexHttpClient } = require("convex/browser");
-require("dotenv").config({ path: ".env.local" });
-const client = new ConvexHttpClient(process.env["CONVEX_URL"]);
-
-client.query("getTasks")().then(console.log);
