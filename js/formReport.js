8c2ad2c82e346dae983515140583b88aa918f67e
Kim-Quirk <kimblackaz@gmail.com>
2023-05-25 23:07:55 -0600

    Fixing bugs

--- js/formReport.js
+++ js/formReport.js
@@ -10,7 +10,6 @@ export default class FormReport {
     init() {
         const formElement = document.forms["reportType"];
         const json = formDataToJSON(formElement);
-        console.log(json)
         this.type = json.reportType;
         if (this.type == "bathroom") {
             this.bathroomForm();
@@ -27,183 +26,33 @@ export default class FormReport {
             this.incidentForm();
         }
     }
-    save() {
-        const formElement = document.forms["bathroomForm"];
-        console.log(formElement)
+    save(formElement) {
+        console.log("In the save function!", formElement)
         const json = formDataToJSON(formElement);
-        console.log(json)
+        console.log(json);
     }
     bathroomForm() {
-        var div = document.querySelector("#remove");
-        div.remove();
-        this.form.setAttribute("name", "bathroomForm");
-        this.form.innerHTML =
-            `<div id="bathroom-appear">
-        <br>
-        <h2>Bathroom Information</h2>
-        <div>
-            <input type="time" class="form-control form-control-lg" />
-        </div>
-        <small class="text-body-secondary">When did they go to the bathroom?</small>
-        <br>
-        <br>
-        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option disabled selected>Select Potty Training Status</option>
-            <option value="1">No Potty Trained / Diapers</option>
-            <option value="2">Currently Potty Training / Pull-ups</option>
-            <option value="3">Potty Training / Underwear</option>
-        </select>
-        <br>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="accident">
-            <label class="form-check-label" for="accident">Select this if the child had an accident.</label>
-        </div>
-        <br>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
-                style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
-        </div>
-        <small class="text-body-secondary">Any additional comments?</small>
-        <br>
-        <br>
-    </div>
-    <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Submit</button>`
-    document.forms["bathroomForm"].addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log(e.target);
-        this.save();
-      });
+        var formOne = document.querySelector("#formOne");
+        formOne.classList.add("d-none");
+        var formTwo = document.querySelector("#bathroomForm");
+        formTwo.classList.remove("d-none");
     }
     foodForm() {
-        var div = document.querySelector("#remove");
-        div.remove();
-        this.form.setAttribute("name", "foodForm");
-        this.form.innerHTML =
-            `<div id="food-appear">
-        <br>
-        <h2>Food Information</h2>
-        <div>
-            <input type="time" class="form-control form-control-lg" />
-        </div>
-        <small class="text-body-secondary">When did they eat?</small>
-        <br>
-        <br>
-        <p>Select Food Type(s):</p>
-        <div class="form-check custom">
-            <input class="form-check-input" type="checkbox" value="vegetables" id="vegetables">
-            <label class="form-check-label" for="vegetables">
-                Vegetables
-            </label>
-        </div>
-        <div class="form-check custom">
-            <input class="form-check-input" type="checkbox" value="fruits" id="fruits">
-            <label class="form-check-label" for="fruits">
-                Fruits
-            </label>
-        </div>
-        <div class="form-check custom">
-            <input class="form-check-input" type="checkbox" value="grains" id="grains">
-            <label class="form-check-label" for="grains">
-                Grains
-            </label>
-        </div>
-        <div class="form-check custom">
-            <input class="form-check-input" type="checkbox" value="protein" id="protein">
-            <label class="form-check-label" for="protein">
-                Protein
-            </label>
-        </div>
-        <div class="form-check custom">
-            <input class="form-check-input" type="checkbox" value="dairy" id="dairy">
-            <label class="form-check-label" for="dairy">
-                Dairy
-            </label>
-        </div>
-        <br>
-        <div class="form-floating">
-            <input type="number" class="form-control" id="num-servings" placeholder="1"
-                value="num-servings">
-            <label for="num-servings">Number of Servings</label>
-        </div>
-        <small class="text-body-secondary">How many servings total did they eat?</small>
-        <br>
-        <br>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
-                style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
-        </div>
-        <small class="text-body-secondary">Any additional comments?</small>
-        <br>
-        <br>
-    </div>
-    <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Submit</button>`
+        var formOne = document.querySelector("#formOne");
+        formOne.classList.add("d-none");
+        var formTwo = document.querySelector("#foodForm");
+        formTwo.classList.remove("d-none");
     }
     incidentForm() {
-        var div = document.querySelector("#remove");
-        div.remove();
-        this.form.setAttribute("name", "incidentForm");
-        this.form.innerHTML =
-            `<div id="incident-appear">
-        <br>
-        <h2>Incident Information</h2>
-        <div>
-            <input type="time" class="form-control form-control-lg" />
-        </div>
-        <small class="text-body-secondary">When did this incident occur?</small>
-        <br>
-        <br>
-        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option disabled selected>Select Incident Type</option>
-            <option value="accident">Potty Accident</option>
-            <option value="misbehavior">Misbehavior</option>
-            <option value="injury">Injury</option>
-            <option value="other">Other</option>
-        </select>
-        <br>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
-                style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Additional Report Information</label>
-        </div>
-        <small class="text-body-secondary">Please fill out a small report here with details about this incident.</small>
-        <br>
-        <br>
-    </div>
-    <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Submit</button>`
+        var formOne = document.querySelector("#formOne");
+        formOne.classList.add("d-none");
+        var formTwo = document.querySelector("#incidentForm");
+        formTwo.classList.remove("d-none");
     }
     napForm() {
-        var div = document.querySelector("#remove");
-        div.remove();
-        this.form.setAttribute("name", "napForm");
-        this.form.innerHTML =
-            `<div id="nap-appear">
-        <br>
-        <h2>Incident Information</h2>
-        <div>
-            <input type="time" class="form-control form-control-lg" />
-        </div>
-        <small class="text-body-secondary">When did the nap time start?</small>
-        <br>
-        <br>
-        <div class="form-floating">
-        <input type="number" class="form-control" id="nap-length" placeholder="1"
-            value="nap-length">
-        <label for="nap-length">Nap length, in minutes</label>
-        </div>
-        <small class="text-body-secondary">How long did they sleep (in minutes)?</small>
-        <br>
-        <br>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
-                style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
-        </div>
-        <small class="text-body-secondary">Any additional comments?</small>
-        <br>
-        <br>
-    </div>
-    <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Submit</button>`
+        var formOne = document.querySelector("#formOne");
+        formOne.classList.add("d-none");
+        var formTwo = document.querySelector("#napForm");
+        formTwo.classList.remove("d-none");
     }
 }
--- js/report.js
+++ js/report.js
@@ -12,8 +12,32 @@ const formReport = new FormReport(form);
 
 // this is how it would look if we listen for the submit on the form
 document.forms["reportType"].addEventListener("submit", (e) => {
-    e.preventDefault();
-    // e.target would contain our form in this case
-    // console.log(e.target);
-    formReport.init();
-  });
+  e.preventDefault();
+  // e.target would contain our form in this case
+  console.log("Form type results", e.target);
+  formReport.init();
+});
+
+document.forms["foodForm"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Saving????")
+  formReport.save(document.forms["foodForm"]);
+});
+
+document.forms["bathroomForm"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Saving????")
+  formReport.save(e.target);
+});
+
+document.forms["napForm"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Saving????")
+  formReport.save(e.target);
+});
+
+document.forms["incidentForm"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Saving????")
+  formReport.save(e.target);
+});
